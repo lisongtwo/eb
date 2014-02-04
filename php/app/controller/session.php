bc94cf5fc552f7e1eafc49221a9404ecdf7c5cf9
lisongtwo <lisongtwo@gmail.com>
2014-02-03 22:13:03 -0800

    signup server side implementation.

--- php/app/controller/session.php
+++ php/app/controller/session.php
@@ -9,24 +9,58 @@
 $urlParts   = preg_split("/\//", $_SERVER["REQUEST_URI"]);
 $reqType    = strtoupper($_SERVER["REQUEST_METHOD"]);
 
-
 //var_dump($urlParts);
 //var_dump($reqType);
 
-if(count($urlParts) == 3 && $reqType == "POST"){
-    if($urlParts[2] == "signup"){
-        $firstName  = $_POST['firstName'];
-        $lastName   = $_POST['lastName'];
-        $email      = $_POST['email'];
-        $password   = $_POST['password'];
+//this is app engine cloud sql connection
+//$db = new mysqli(null, "root", "", null, null, "/cloudsql/august-period-412:form");
+//this is the local mysql db connection
+$db = new mysqli("localhost", "root", "", "main");
+if($db->connect_errorno > 0){
+    die('Unable to connect database [' . $db->connect_error . ']');
+}
 
-        echo json_encode(array('firstName'=>$firstName, 'lastName'=>$lastName));
-
-    }elseif($urlParts[2] == "login"){
-        echo "to login";
+try{
+    if(count($urlParts) == 3 && $reqType == "POST" && $urlParts[2] == 'signup'){
+        toSignup();
+    }else if(count($urlParts) == 3 && $reqType == "POST" && $urlParts[2] == 'login'){
+        toLogin();
     }else{
-        echo "wrong action type in /session";
+        echo 'wrong action';
     }
-}else{
-    echo "wrong parameter in /session";
+}catch(Exception $e){
+    echo json_encode(array('status'=>'fail', 'id'=>0, 'message'=>$e->getMessage()));
+}
+
+
+function toSignup(){
+    global $db;
+
+    $firstName  = $_POST['firstName'];
+    $lastName   = $_POST['lastName'];
+    $email      = $_POST['email'];
+    $password   = md5($_POST['password']);
+
+    //duplication check
+    $query = sprintf("SELECT COUNT(*) AS total FROM user WHERE email='%s'", mysql_real_escape_string($email));
+    $result = $db->query($query);
+    if($result){
+        $numberOfExistingEmail = $result->fetch_object()->total;
+        if($numberOfExistingEmail > 0){
+            throw new Exception('Email is registered already');
+        }
+    }
+
+    //create the new user
+    $query = sprintf("INSERT INTO user (firstName, lastName, email, password) values ('%s', '%s', '%s', '%s')",
+                        mysql_real_escape_string($firstName),
+                        mysql_real_escape_string($lastName),
+                        mysql_real_escape_string($email),
+                        mysql_real_escape_string($password));
+    $db->query($query);
+    echo json_encode(array('status'=>'success', 'id'=>$db->insert_id, 'message'=>''));
+}
+
+function toLogin(){
+    echo json_encode(array('status'=>'success', 'id'=>0, 'message'=>''));
 }
